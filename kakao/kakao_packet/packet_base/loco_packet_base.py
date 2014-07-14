68c0ffe1eb1a8ed734783f49b65fe43dba14c0c0
joke111 <alta.second@gmail.com>
2014-07-14 10:48:59 +0900

    Add rsa logic

--- kakao/kakao_packet/packet_base/loco_packet_base.py
+++ kakao/kakao_packet/packet_base/loco_packet_base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import rsa
+
 class LocoPacketBase:
 	def __init__(self):
 		pass
@@ -8,9 +10,12 @@ class LocoPacketBase:
 		pass
 
 	def encrypt_by_aes(self, data):
-		#TODO
 		pass
 
 	def encrypt_by_rsa(self, data):
-		#TODO
-		pass
+		N = 0xaf0dddb4de63c066808f08b441349ac0d34c57c499b89b2640fd357e5f4783bfa7b808af199d48a37c67155d77f063ddc356ebf15157d97f5eb601edc5a104fffcc8895cf9e46a40304ae1c6e44d0bcc2359221d28f757f859feccf07c13377eec2bf6ac2cdd3d13078ab6da289a236342599f07ffc1d3ef377d3181ce24c719
+		E = 3
+
+		public_key = rsa.PublicKey(N, E)
+
+		return rsa.encrypt(data, public_key)
