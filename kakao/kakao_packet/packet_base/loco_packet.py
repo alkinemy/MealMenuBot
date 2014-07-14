453b05e83f30918d34f12c6d2dbe4bb8e45d46c8
joke111 <alta.second@gmail.com>
2014-07-14 13:15:04 +0900

    Add LocoPacket Logic

--- kakao/kakao_packet/packet_base/loco_packet.py
+++ kakao/kakao_packet/packet_base/loco_packet.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
+import sys
 from loco_packet_base import LocoPacketBase
+
+try:
+	from bson import BSON
+except ImportError, e:
+	print ("Import Error %s", e)
+	sys.exit()
 
 class LocoPacket(LocoPacketBase):
 	def __init__(self):
@@ -8,4 +15,11 @@ class LocoPacket(LocoPacketBase):
 
 	#Override
 	def create(self, command, args):
-		pass
+		packet_id = "\xFF\xFF\xFF\xFF"
+		status_code = "\x00\x00"
+		method = command + ("\x00" * (11 - len(command)))
+		body_type = "\x00"
+		body_contents = BSON.encode(args)
+		body_length = body_contents[:4]
+
+		return packet_id + status_code + method + body_type + body_lentgh + body_contents
