4fe15d9fbeca8c951b87f413992dfc54194a3619
joke111 <alta.second@gmail.com>
2014-07-14 09:40:17 +0900

    Add skeleton of base packet

--- kakao/kakao_packet/packet_base/loco_packet_base.py
+++ kakao/kakao_packet/packet_base/loco_packet_base.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python
+
+class LocoPacketBase:
+	def __init__(self):
+		pass
+
+	def create(self, command, args):
+		pass
+
+	def encrypt_by_aes(self, data):
+		#TODO
+		pass
+
+	def decrypt_by_aes(self, data):
+		#TODO
+		pass
+
+	def encrypt_by_rsa(self, data):
+		#TODO
+		pass
