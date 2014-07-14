8c97a3c8383313e42adb1b1d192bfa14f4100455
joke111 <alta.second@gmail.com>
2014-07-14 13:53:15 +0900

    Add LocoSecureHandshakePacket logic

--- kakao/kakao_packet/packet_base/loco_secure_handshake_packet.py
+++ kakao/kakao_packet/packet_base/loco_secure_handshake_packet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from loco_packet_base import LocoPacketBase
+from loco_secure_normal_packet import LocoSecureNormalPacket
 
 class LocoSecureHandshakePacket(LocoPacketBase):
 	def __init__(self):
@@ -8,4 +9,11 @@ class LocoSecureHandshakePacket(LocoPacketBase):
 
 	#Override
 	def create(self, command, args):
-		pass
+		aes_key = "\x00" * 16 #TODO fix
+
+		handshake = "\x80\x00\x00\x00"
+		handshake += "\x01\x00\x00\x00"
+		handshake += "\x01\x00\x00\x00"
+		handshake += self.encrypt_rsa(aes_key)
+
+		return (handshake + LocoSecureNormalPacket().create(command, args))
