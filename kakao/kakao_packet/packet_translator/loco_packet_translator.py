342c69709f6f47b7e56b6673150232599adb833c
joke111 <alta.second@gmail.com>
2014-07-15 18:43:13 +0900

    Add translating logic

--- kakao/kakao_packet/packet_translator/loco_packet_translator.py
+++ kakao/kakao_packet/packet_translator/loco_packet_translator.py
@@ -16,9 +16,9 @@ sys.path.append(os.path.abspath("../kakao_config"))
 import loco_config
 
 class LocoPacketTranslator:
+	#TODO
+	#kakao main machine에 translate function을 만들고 안에서 소켓이 null인지 확인해야함
 	def receive_and_translate(self, socket, force_reply=False):
-		#TODO
-		#kakao main machine에 translate function을 만들고 안에서 소켓이 null인지 확인해야함
 		head = socket.recv(4)
 		
 		if (self.__is_connection_closed(head)):
@@ -48,40 +48,35 @@ class LocoPacketTranslator:
 
 		#receive and decrypt packet
 		body = self.__receive_and_decrypt_by_aes(head, socket)
-
 		entire_body_length = struct.unpack("I", entire_body[18:22])[0]
 		recv_entire_body_length = len(body[22:])
 
-		#내부 패킷에 대해서 패킷 프로세싱
+		#다른 패킷이 넘어오면 그것에 대한 처리?
 		while (recv_entire_body_length < entire_body_length):
 			body = self.__receive_and_decrypt_by_aes(socket.recv(4), socket)
 			entire_body += body
 			recv_entire_body_length += len(body)
 
 		result = self.__translate_hexcode(entire_body)
-		result["body_contents"] = decode_all(entire_body[22:])[0]
 
 		#만약에 loco_packet이 아니고 force_reply면 다시 receive_translate시작
-
-		if (not self.__is_loco_packet and force_reply):
+		if (not self.__is_loco_packet(result["packet_id"]) and force_reply):
 			#Handle non-loco-packet
 			return self.receive_and_translate(socket, force_reply)
 		else:
 			return result
 
-
 	def __receive_and_decrypt_by_aes(self, head, socket):
-		aes_encrypted_data_length = struct.unpack("I", head)[0]
 		aes_encrypted_data = ""
+		aes_encrypted_data_length = struct.unpack("I", head)[0]
 		received_aes_encrypted_data_length = 0
 
-		while(received_encrypted_data_length < aes_encrypted_data_length):
-			received = socket.recv(aes_encrypted_data_length - received_encrypted_data_length)
+		while(received_aes_encrypted_data_length < aes_encrypted_data_length):
+			received = socket.recv(aes_encrypted_data_length - received_aes_encrypted_data_length)
 			aes_encrypted_data += received
 			received_aes_encrypted_data_length += len(received)
 		
 		return self.__decrypt_by_aes(aes_encrypted_data)
-
 	
 	def __translate_hexcode(self, hexcode):
 		result = self.__translate_packet_header(hexcode)
